5f2f2b9f8aedf20c3fc577410e296b4393dbde9a
xo482 <122208264+xo482@users.noreply.github.com>
2024-03-04 13:51:21 +0900

    [Bronze II] Title: 소수, Time: 164 ms, Memory: 14896 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int x = Integer.parseInt(br.readLine());
+        int y = Integer.parseInt(br.readLine());
+        int sum = 0;
+        int min = -1;
+        boolean pass = false;
+        
+        
+        for(int i=x; i<=y; i++){
+            if(i == 1) pass = true;
+            for(int j=2;j<i;j++){
+                if(i % j == 0) {
+                    pass = true;
+                    break;
+                }
+            }
+            
+            if(pass == false){ 
+                if(sum == 0) min = i;
+                sum += i;
+            }
+            
+            if(pass == true)
+                pass = false;
+        }
+        
+        if(sum != 0){
+            System.out.println(sum);
+        }
+        System.out.print(min);
+	}
+}
+
+
+
+
+
