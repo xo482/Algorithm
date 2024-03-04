3fcd7b76dac2aebae2cfe7262af4f5983b962da5
xo482 <122208264+xo482@users.noreply.github.com>
2024-03-04 14:20:40 +0900

    [Bronze I] Title: 소인수분해, Time: 156 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -5,12 +5,12 @@ public class Main {
 	public static void main(String[] args) throws IOException {
 	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int len = N;
-        for(int i=2; i<=len; i++) {
+        int i = 2;
+        while(N > 1){
             if(N % i == 0){
                 N = N / i;
-                System.out.println(i--);
-            }
+                System.out.println(i);
+            } else i++;
         }
 	}
 }
