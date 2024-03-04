f6081a39a111235a36e948b65006b504c78a3ca4
xo482 <122208264+xo482@users.noreply.github.com>
2024-03-04 13:26:36 +0900

    [Bronze V] Title: 사칙연산, Time: 216 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x, y;
+        x = sc.nextInt();
+        y = sc.nextInt();
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+        System.out.println(x%y);
+    }
+}
