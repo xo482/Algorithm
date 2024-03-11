a0696806a078e4b03a6a76f00acd06e4d0ad9425
xo482 <122208264+xo482@users.noreply.github.com>
2024-03-11 16:20:14 +0900

    [Gold IV] Title: 플로이드, Time: 416 ms, Memory: 42388 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.java
+++ 백준/Gold/11404. 플로이드/플로이드.java
@@ -1,67 +1,45 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
-    static int cities;
-    static int buses;
-    static int[][] busList;
-    static boolean[][] check;
-
-    public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        cities = Integer.parseInt(br.readLine());
-        buses = Integer.parseInt(br.readLine());
-        busList = new int[cities][cities];
-        check = new boolean[cities][cities];
-
-        // init
-        StringTokenizer st = null;
-        for (int i = 0; i < buses; i++) {
-            st = new StringTokenizer(br.readLine());
-            int y = Integer.parseInt(st.nextToken()) - 1;
-            int x = Integer.parseInt(st.nextToken()) - 1;
-            int cost = Integer.parseInt(st.nextToken());
-            if (check[y][x]) {
-                if (busList[y][x] > cost) {
-                    busList[y][x] = cost;
-                }
-            } else {
-                busList[y][x] = cost;
-                check[y][x] = true;
-            }
-        }
-
-        // logic
-        for (int x = 0; x < cities; x++) {
-            for (int i = 0; i < cities; i++) {
-                for (int j = 0; j < cities; j++) {
-                    if (check[i][j]) {
-                        function(i, j);
-                    }
+    public static void solution(int[][] arr) {
+        for(int k = 0; k < arr.length; k++) {
+            for(int i = 0; i < arr.length; i++) {
+                for(int j = 0; j < arr.length; j++) {
+                    if(k == i || k == j) continue;
+                    arr[i][j] = Math.min(arr[i][j], arr[i][k] + arr[k][j]);
                 }
             }
-        }
-
-        for (int[] ints : busList) {
-            for (int anInt : ints) {
-                System.out.print(anInt+" ");
-            }
-            System.out.println();
         }
     }
 
-    private static void function(int i, int j) {
-        for (int k = 0; k <cities; k++) {
-            if (check[j][k] && (i!=k)) {
-                if (check[i][k]){
-                    busList[i][k] = Math.min(busList[i][k], busList[i][j] + busList[j][k]);
-                } else {
-                    busList[i][k] = busList[i][j] + busList[j][k];
-                    check[i][k] = true;
-                }
-            }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][n];
+        for(int i = 0; i < arr.length; i++) {
+            Arrays.fill(arr[i], 10000000);
+            arr[i][i] = 0;
         }
+        for(int i = 0; i < m; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken());
+            arr[a][b] = Math.min(arr[a][b], c);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        solution(arr);
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = 0; j < arr[i].length; j++) {
+                if(arr[i][j] == 10000000) sb.append(0).append(" ");
+                else sb.append(arr[i][j]).append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb);
     }
 }
