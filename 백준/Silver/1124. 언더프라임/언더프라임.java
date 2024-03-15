fa6cfcd8bdca188a990975c91ca8d987447000be
xo482 <122208264+xo482@users.noreply.github.com>
2024-03-16 02:02:53 +0900

    [Silver I] Title: 언더프라임, Time: 160 ms, Memory: 15308 KB -BaekjoonHub

--- 백준/Silver/1124. 언더프라임/언더프라임.java
+++ 백준/Silver/1124. 언더프라임/언더프라임.java
@@ -1,42 +1,51 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
-public class Main {
-    public static void main(String[] args) throws IOException {
+class Main{
+    
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] s = br.readLine().split(" ");
-
-        int A = Integer.parseInt(s[0]);
-        int B = Integer.parseInt(s[1]);
-        int underPrime = 0;
-
-        for (int i = A; i <= B; i++) {
-            int now = i;
-            int cnt = 0;
-            int divider = 2;
-
-            while (now > 1) {
-                if (i == divider) break;
-                if (now % divider == 0) {
-                    now /= divider;
-                    cnt++;
-                } else divider++;
-            }
-
-            if (cnt < 2) continue;
-
-            // 소수의 개수가 소수인지 확인
-            underPrime++;
-            for (int j = 2; j < cnt; j++) {
-                if (cnt % j == 0) {
-                    underPrime--;
-                    break;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        
+        // 인덱스에 해당하는 숫자가 -> false: 소수 , true: 합성수
+        boolean[] prime = new boolean[100001];
+        
+        // 인덱스에 해당하는 숫자의 소수의 개수를 담을 리스트
+        int[] count = new int[100001];
+        
+        // 0, 1은 소수가 될 수 없음
+        prime[0] = prime[1] = true;
+        
+        for(int i = 2; i < 100001; i++){
+            // 만약 true면 이미 합성수라는 뜻임
+            if(prime[i]) continue;
+            
+            // 위의 조건문을 지나왔다면 일단 i는 소수임
+            for(int j = i+i; j < 100001; j += i){
+                // 해당하는 소수의 배수들은 전부 합성수이므로 true로 만들어줌
+                // 이후에 그 수가 i의 값이 된다면 위의 조건문에 걸려서 지나감
+                prime[j] = true;
+                
+                // 제곱 근의 개수만큼 리스트 값 증가시킴
+                // j는 그 수만큼 i를 가지고 있음
+                // ex) j=169, i=13일 때 j는 i를 인수로 2개 가짐
+                int tmp = j;
+                while(tmp % i == 0){
+                    tmp /= i;
+                    count[j]++;
                 }
             }
         }
-
-        System.out.println(underPrime);
-
+        
+        // i의 소수의 개수를 가져오고, 그 수가 소수인지 확인
+        int cnt = 0;
+        for(int i = a; i <= b; i++){
+            if(!prime[count[i]]) cnt++;
+        }
+        
+        System.out.println(cnt);
     }
 }
