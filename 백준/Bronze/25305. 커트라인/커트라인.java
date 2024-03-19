0701f2d83cd23b20584ba36d2cec7770859ff043
xo482 <122208264+xo482@users.noreply.github.com>
2024-03-19 09:35:15 +0900

    [Bronze II] Title: 커트라인, Time: 140 ms, Memory: 14396 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split(" ");
+        int N = Integer.parseInt(str[0]);
+        int k = Integer.parseInt(str[1]);
+        List<Integer> list = new ArrayList<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(list);
+
+        System.out.println(list.get(N-k));
+    }
+}
