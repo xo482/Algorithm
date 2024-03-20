a41221081feeba0765a563997653d95fcc702e1e
xo482 <122208264+xo482@users.noreply.github.com>
2024-03-20 13:04:49 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 192 ms, Memory: 16352 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.java
@@ -1,24 +1,23 @@
-import java.io.*;
-import java.util.*;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
-public class Main {
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int quarter=0,dime=0, nickel=0, penny=0;
-        int length = Integer.parseInt(br.readLine());
-        int charge=0;
-        
-        for(int i=0;i<length;i++){
-            charge = Integer.parseInt(br.readLine());
-            quarter = charge / 25;
-            charge = charge % 25;
-            dime = charge / 10;
-            charge = charge % 10;
-            nickel = charge / 5;
-            penny = charge % 5;
-            
-            System.out.println(quarter + " " + dime + " " + nickel + " " + penny);
+public class Main{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //BufferedWriter bw = new Bufferedwriter(new OutputStreamWriter(System.in));
+        int T = Integer.parseInt(br.readLine());
+        //순서대로 쿼터, 다임, 니켈, 페니
+        int q,d,n,p = 0;
+        for(int i=0; i<T; i++){
+            int C = Integer.parseInt(br.readLine());
+            q = C/25;
+            d = (C-(25*q))/10;
+            n = (C-(25*q+10*d))/5;
+            p = (C-(25*q+10*d+5*n))/1;
+            System.out.println(q + " " + d + " " + n + " " + p);
         }
-        
-	}
+    }
 }
