86bb73f99d66e462ea1e50b5e3501d3532a299f3
xo482 <122208264+xo482@users.noreply.github.com>
2024-03-27 09:45:58 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 648 ms, Memory: 56484 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        Map<String, String> map = new HashMap<>();
+        
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        
+        while(N-- > 0){
+            st = new StringTokenizer(br.readLine());
+            map.put(st.nextToken(), st.nextToken());
+        }
+        
+        while(M-- > 0){
+            sb.append(map.get(br.readLine())).append("\n");
+        }
+        
+        System.out.print(sb);
+	}
+}
