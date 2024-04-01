c72f04b827cc5d0ce157ed3462b8303be4cd01c2
xo482 <122208264+xo482@users.noreply.github.com>
2024-04-01 16:49:17 +0900

    [Silver III] Title: N과 M (8), Time: 164 ms, Memory: 16672 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] arr;
+    static int[] list;
+    static int N;
+    static int M;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        list = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < N; i++) {
+            list[0] = arr[i];
+            solution(i, 1,list);
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void solution(int start, int m, int[] list) {
+        if (m == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(list[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <N; i++) {
+            list[m] = arr[i];
+            solution(i, m+1, list);
+        }
+    }
+}
