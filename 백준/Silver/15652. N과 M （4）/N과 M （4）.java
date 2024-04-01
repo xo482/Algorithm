e3258b59c0b6812249dd8f7694875e6e65c476fe
xo482 <122208264+xo482@users.noreply.github.com>
2024-04-01 17:04:38 +0900

    [Silver III] Title: N과 M (4), Time: 160 ms, Memory: 16724 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int[] arr;
+    static int[] list;
+    static int N;
+    static int M;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+        list = new int[M];
+
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = i+1;
+        }
+
+
+        solution(0,0,list);
+        System.out.println(sb);
+    }
+
+    private static void solution(int start, int m, int[] list) {
+        if (m == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(list[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <N; i++) {
+            list[m] = arr[i];
+            solution(i, m+1, list);
+        }
+    }
+}
