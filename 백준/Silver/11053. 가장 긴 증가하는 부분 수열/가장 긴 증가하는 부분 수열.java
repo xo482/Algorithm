8b5d9d3268e08781ac86f263d7a6ceb809fd1b40
xo482 <122208264+xo482@users.noreply.github.com>
2024-04-07 23:49:18 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 144 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -4,17 +4,12 @@ import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-
-
-    static Map<Integer, Integer> map = new HashMap<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()); // 배열의 길이
         int[] arr = new int[N];
         int[] dp = new int[N];
-
 
         // init
         st = new StringTokenizer(br.readLine());
@@ -23,18 +18,13 @@ public class Main {
             dp[i] = 1;
         }
 
-        for (int i = N-2; i >= 0; i--) {
-            for (int j = i+1; j <N; j++) {
+        for (int i = N-2; i >= 0; i--) 
+            for (int j = i+1; j <N; j++) 
                 if (arr[i] < arr[j] && dp[i] < dp[j] + 1)
                     dp[i] = dp[j] + 1;
-            }
-        }
-
+              
         int max = 0;
-        for (int i : dp) {
-            max = Math.max(max, i);
-        }
-
+        for (int i : dp) max = Math.max(max, i);     
         System.out.println(max);
     }
 
