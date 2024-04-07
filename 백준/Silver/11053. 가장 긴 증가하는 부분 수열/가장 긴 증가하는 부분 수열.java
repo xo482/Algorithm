9ba9b6e53674cfd2dd99bb1eca2b5b5ae0c61913
xo482 <122208264+xo482@users.noreply.github.com>
2024-04-07 23:48:00 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 144 ms, Memory: 14668 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -5,36 +5,37 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int N; // 배열의 길이
-    static int[] arr;
+
     static Map<Integer, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        arr = new int[N];
+        int N = Integer.parseInt(st.nextToken()); // 배열의 길이
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+
 
         // init
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
-            map.put(i, 0);
+            dp[i] = 1;
         }
 
         for (int i = N-2; i >= 0; i--) {
             for (int j = i+1; j <N; j++) {
-                if (arr[i] < arr[j] && map.get(i) < map.get(j)+1) map.put(i, map.get(j) + 1);
+                if (arr[i] < arr[j] && dp[i] < dp[j] + 1)
+                    dp[i] = dp[j] + 1;
             }
         }
 
         int max = 0;
-        for (Integer i : map.keySet()) {
-            int val = map.get(i);
-            if (max < val) max = val;
+        for (int i : dp) {
+            max = Math.max(max, i);
         }
 
-        System.out.println(max + 1);
+        System.out.println(max);
     }
 
 }
