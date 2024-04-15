18bfe1951c3422ab39d9fb803529456996f82499
xo482 <122208264+xo482@users.noreply.github.com>
2024-04-15 16:23:07 +0900

    [Silver III] Title: 달팽이, Time: 520 ms, Memory: 48564 KB -BaekjoonHub

--- 백준/Silver/1913. 달팽이/달팽이.java
+++ 백준/Silver/1913. 달팽이/달팽이.java
@@ -10,52 +10,51 @@ public class Main {
     static int I = 0;
     static int J = 0;
     static int[][] arr;
-    static int[] dr = new int[] {-1, 0, 1, 0};
+    static int[] dr = new int[] {1, 0, -1, 0};
     static int[] dc = new int[] {0, 1, 0, -1};
     static int index = 0;
+    static int target;
     static int number;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        number = Integer.parseInt(br.readLine());
+        target = Integer.parseInt(br.readLine());
         arr = new int[N][N];
+        number = N*N;
 
-        int i = N/2; // r
-        int j = N/2; // c
-        arr[i][j] = 1;
-
-        int cnt = 1; // 한번에 진행하는 이동 수
-        int ele = 1; // 각 인덱스에 들어갈 값
-        while (i != -1) {
-            for (int k = 0; k < 2; k++) {
-                for (int l = 0; l < cnt; l++) {
-                    if (number == ele) {
-                        I = i+1;
-                        J = j+1;
-                    }
-                    arr[i][j] = ele++;
-                    i += dr[index];
-                    j += dc[index];
-                    if (i == -1) break;
-                }
-                if (i == -1) break;
-                turn();
+        int i = 0;
+        int j = 0;
+        while(number != 0){
+            if (number == target) {
+                I = i+1;
+                J = j+1;
             }
-            cnt++;
-        }
+            arr[i][j] = number--;
 
+            if (check(i + dr[index], j + dc[index])) {
+                i += dr[index];
+                j += dc[index];
+            } else {
+                turn();
+                i += dr[index];
+                j += dc[index];
+            }
+        }
 
         for (int[] ints : arr) {
             for (int anInt : ints) sb.append(anInt).append(" ");
             sb.append("\n");
         }
         sb.append(I).append(" ").append(J);
-
         System.out.print(sb);
     }
 
     private static void turn() {
         index++;
         index %= 4;
+    }
+    private static boolean check(int i, int j) {
+        if (i >= 0 && j >= 0 && i < N && j < N && arr[i][j] == 0) return true;
+        return false;
     }
 }
