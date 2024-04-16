3cd082d622d9e60d05ce50e8c225d5794d55a9f9
xo482 <122208264+xo482@users.noreply.github.com>
2024-04-16 16:06:50 +0900

    [Silver II] Title: 최소 힙, Time: 336 ms, Memory: 26260 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int N;
+    static Queue<Integer> queue = new PriorityQueue<>();
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                if (!queue.isEmpty()) sb.append(queue.remove()).append("\n");
+                else sb.append("0\n");
+            } else {
+                queue.add(num);
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
+
