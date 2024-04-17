b018ce7bb44fe4e1cd5bb5249cc83e7851668ec9
xo482 <122208264+xo482@users.noreply.github.com>
2024-04-17 21:35:25 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 18172 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -6,28 +6,15 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] dp = new int[1_000_001];
-        int a, b, c, d;
+        int[] dp = new int[N+1];
         dp[1] = 0;
-        dp[2] = 1;
-        dp[3] = 1;
 
-        for (int i=4; i < 1_000_001; i++){
-            if (i % 3 == 0){
-                c = dp[i] = dp[i/3] + 1;
-            } else {
-                c = Integer.MAX_VALUE;
-            }
-
-            if (i % 2 == 0) {
-                d = dp[i] = dp[i / 2] + 1;
-            } else {
-                d = Integer.MAX_VALUE;
-            }
-
-            a = dp[i-1] + 1;
-            b = dp[i-2] + 2;
-            dp[i] = Math.min(Math.min(a,b), Math.min(c,d));
+        for (int i=2; i < N+1; i++){
+            dp[i] = dp[i-1] + 1;
+            if (i % 3 == 0)
+                dp[i] = Math.min(dp[i], dp[i/3] + 1);
+            if (i % 2 == 0) 
+                dp[i] = Math.min(dp[i], dp[i/2] + 1);
         }
 
         System.out.print(dp[N]);
