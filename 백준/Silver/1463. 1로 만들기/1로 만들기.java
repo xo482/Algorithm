33484e34a1c290f091166454508a2932a49060c4
xo482 <122208264+xo482@users.noreply.github.com>
2024-04-22 16:05:49 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 18232 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -7,7 +7,6 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] dp = new int[N+1];
-        dp[1] = 0;
 
         for (int i=2; i < N+1; i++){
             dp[i] = dp[i-1] + 1;
