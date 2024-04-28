699bf320744976363e72d2f3ed7e0178a3f1b9ee
xo482 <122208264+xo482@users.noreply.github.com>
2024-04-28 22:42:19 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        for(int i : array) 
+            if (height < i) answer++;
+        
+        return answer;
+    }
+}
