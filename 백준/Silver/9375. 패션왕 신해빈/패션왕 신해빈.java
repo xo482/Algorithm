5b9ebdbfa855fdfdd779608232f979abcb5e6df1
xo482 <122208264+xo482@users.noreply.github.com>
2024-05-09 10:08:49 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 124 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -10,25 +10,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         T = Integer.parseInt(br.readLine());
-
         while (T-- > 0) {
             Map<String, Integer> map = new HashMap<>();
             int n = Integer.parseInt(br.readLine());
+
             for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
                 st.nextToken();
                 String str = st.nextToken();
-                if (map.containsKey(str)) map.replace(str, map.get(str) + 1);
-                else map.put(str, 1);
+                map.put(str, map.getOrDefault(str, 0) + 1);
             }
 
             int cnt = 1;
-            for (int i : map.values()) {
-                cnt *= (i+1);
-            }
+            for (int i : map.values()) cnt *= (i+1);
             sb.append(cnt - 1).append("\n");
         }
-
         System.out.print(sb);
     }
 }
