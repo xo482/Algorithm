865a1ca3858f2f19f33ca350e1c8f3428f25c0aa
xo482 <122208264+xo482@users.noreply.github.com>
2024-07-08 15:44:06 +0900

    [Silver III] Title: N과 M (1), Time: 240 ms, Memory: 22804 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,33 +1,30 @@
+import java.util.*;
 import java.io.*;
 
 public class Main {
 
+    static int N;
+    static int M;
     static int[] arr;
-    static boolean[] visit;
-    static int N,M;
+    static boolean[] visited;
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = br.readLine().split(" ");
+        N = Integer.parseInt(s[0]);
+        M = Integer.parseInt(s[1]);
 
-        String[] str = br.readLine().split(" ");
-        // 노드의 개수
-        N = Integer.parseInt(str[0]);
-
-        // 방문 가능한 노드의 최대 길이
-        M = Integer.parseInt(str[1]);
-
+        visited = new boolean[N];
         arr = new int[M];
-        visit = new boolean[N];
 
-        dfs(0);
-
-        System.out.print(sb);
+        tracking(0);
+        System.out.println(sb);
     }
 
-    private static void dfs(int depth) {
-
-        if (depth == M){
+    private static void tracking(int cnt) {
+        if (cnt == M) {
             for (int i : arr) {
                 sb.append(i).append(" ");
             }
@@ -36,11 +33,11 @@ public class Main {
         }
 
         for (int i = 0; i < N; i++) {
-            if (!visit[i]) {
-                visit[i] = true;
-                arr[depth] = i+1;
-                dfs(depth+1);
-                visit[i] = false;
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[cnt] = i + 1;
+                tracking(cnt+1);
+                visited[i] = false;
             }
         }
     }
