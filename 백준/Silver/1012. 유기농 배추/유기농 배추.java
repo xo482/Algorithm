6e2d83aa093d9de341e41e81f2d45eb5349cc8d1
xo482 <122208264+xo482@users.noreply.github.com>
2024-07-11 14:20:21 +0900

    [Silver II] Title: 유기농 배추, Time: 128 ms, Memory: 15968 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -2,70 +2,62 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int dirX[] = {0, 0, -1, 1};
-	static int dirY[] = {-1, 1, 0, 0};
-	static int map[][];
-	static boolean visit[][];
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int T, N, M, K, r, c, answer;
+    static boolean[][] field;
 
-	static int now_x, now_y;
-	static int M, N, K;
-	static int count;
+    static int[] dr = new int[]{-1, 1, 0, 0};
+    static int[] dc = new int[]{0, 0, -1, 1};
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
 
-		int T = Integer.parseInt(br.readLine());
-		for(int i=0; i<T; i++) {
-			st = new StringTokenizer(br.readLine());
+        T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
 
-			M = Integer.parseInt(st.nextToken());
-			N = Integer.parseInt(st.nextToken());
-			K = Integer.parseInt(st.nextToken());
+            // 초기화
+            answer = 0;
+            field = new boolean[N][M];
+            while (K-- > 0) {
+                st = new StringTokenizer(br.readLine());
+                c = Integer.parseInt(st.nextToken());
+                r = Integer.parseInt(st.nextToken());
+                field[r][c] = true;
+            }
 
-			map = new int[N][M];
-			visit = new boolean[N][M];
+            // 탐색
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (field[i][j]) {
+                        dfs(new int[]{i, j});
+                        answer++;
+                    }
+                }
+            }
 
-			for(int j=0; j<K; j++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				map[y][x] = 1;
-			}
+            sb.append(answer).append("\n");
+        }
 
-			count = 0;
-			for(int j=0; j<N; j++) {
-				for(int k=0; k<M; k++) {
+        System.out.print(sb);
+    }
 
-					if(map[j][k] == 1 && visit[j][k] == false) {
-						count++;
-						DFS(k, j);
-					}
-				}
-			}
-			sb.append(count).append('\n');
-		}
+    private static void dfs(int[] ints) {
+        int r = ints[0];
+        int c = ints[1];
 
-		System.out.println(sb);
-	}
-	
-	public static void DFS(int x, int y) {
-		visit[y][x] = true;
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
 
-		for(int i=0; i<4; i++) {
-			now_x = x + dirX[i];
-			now_y = y + dirY[i];
-
-			if(Range_check() && visit[now_y][now_x] == false && map[now_y][now_x] == 1) {
-				DFS(now_x, now_y);
-			}
-
-		}
-	}
-
-	static boolean Range_check() {
-		return (now_y < N && now_y >= 0 && now_x < M && now_x >= 0);
-	}
-
+            if (nr >= 0 && nr < N && nc >= 0 && nc < M && field[nr][nc]){
+                field[nr][nc] = false;
+                dfs(new int[]{nr, nc});
+            }
+        }
+    }
 }
