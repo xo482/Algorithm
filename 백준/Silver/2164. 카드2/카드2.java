f49b210d6b24f1b2a4e27ad38999cae4c717e309
xo482 <122208264+xo482@users.noreply.github.com>
2024-07-15 15:28:40 +0900

    [Silver IV] Title: 카드2, Time: 176 ms, Memory: 36844 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,28 +1,24 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayDeque;
 
 public class Main {
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static int N;
-    static boolean flag = true;
-    static ArrayDeque<Integer> deque = new ArrayDeque<>();
-
     public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        for (int i = 1; i <= N; i++) deque.addLast(i);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayDeque<Integer> Q = new ArrayDeque<>();
+        boolean flag = false;
+        int answer = 0;
 
-        while (deque.size() > 1) {
-            if (flag) {
-                deque.removeFirst();
-                flag = false;
-            } else {
-                deque.addLast(deque.removeFirst());
-                flag = true;
-            }
+        for (int i = 1; i < N + 1; i++) Q.addLast(i);
+
+        while (!Q.isEmpty()) {
+            answer = Q.removeFirst();
+
+            if (flag) Q.addLast(answer);
+            flag = !flag;
         }
-        System.out.print(deque.removeFirst());
+
+        System.out.print(answer);
     }
 }
