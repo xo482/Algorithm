22c1ec824ce82be1436fed9f9c197e2e916b3511
xo482 <122208264+xo482@users.noreply.github.com>
2024-09-15 21:51:26 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.java
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -1,19 +1,22 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int x=1, i=1;
-        int range=1;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static double N;
+
+    public static void main(String[] args) throws IOException {
+        N = Double.parseDouble(br.readLine());
+        N = (N-1) / 6;
+
+        int cnt = 0;
+        int sum = 0;
+
+        while (sum < N) sum += ++cnt;
         
-        while(N > range){
-            range = 6*x + 1;
-            i++;
-            x += i;
-        }
-        
-        System.out.println(i);
-	}
+        System.out.println(cnt+1);
+    }
 }
+// 1 7 19 37 61...
+// 0 6 18 36 60...
+// 0 1 3 6 10... 배수
+// 1더하기 2더하기 3더하기 4더하기...
