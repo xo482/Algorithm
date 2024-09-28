0e7c0de25f5e87e344ae5f14eb64a20df04d8639
xo482 <122208264+xo482@users.noreply.github.com>
2024-09-28 21:34:13 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,29 +1,30 @@
- import java.io.*;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int[] list = new int[N];
-        int max = 0, val=0;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+    static int N, M, max = -1;
+    static int[] arr;
+    
+    public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
-        for(int i=0; i<N; i++) {
-            list[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        for(int i=0; i<N-2; i++){
-            for(int j=i+1; j< N-1;j++){
-                for(int k = j+1; k < N; k++) {
-                    val = list[i] + list[j] + list[k];
-                    if(max < val && val <= M) max = val;
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N-2; i++) {
+            for (int j = i+1; j < N-1; j++) {
+                for (int k = j+1; k < N; k++) {
+                    int sum = arr[i] + arr[j] + arr[k];
+                    if (sum <= M) max = Math.max(max, sum);
                 }
             }
         }
         
-        System.out.print(max);
-	}
+        System.out.println(max);
+    }
 }
