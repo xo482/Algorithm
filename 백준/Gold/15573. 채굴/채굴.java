cf45fbb8019e4118e720fb326de49f5f9f8df0d0
xo482 <122208264+xo482@users.noreply.github.com>
2024-10-09 16:57:20 +0900

    [Gold III] Title: 채굴, Time: 1720 ms, Memory: 310924 KB -BaekjoonHub

--- 백준/Gold/15573. 채굴/채굴.java
+++ 백준/Gold/15573. 채굴/채굴.java
@@ -31,34 +31,18 @@ public class Main {
         // 채굴기의 강도를 이분탐색
         int start = min;
         int end = max;
-        int mid = -1;
+        int sum = -1;
 
-//        System.out.println("start = " + start);
-//        System.out.println("end = " + end);
+        while (start < end) {
+            int mid = (start + end) / 2;
+            sum = bfs(mid);
 
-        while (start <= end) {
-            mid = (start + end) / 2;
-            int sum = bfs(mid);
-//            System.out.print("채굴기의 강도 = " + mid);
-//            System.out.println(", 캔 광물 수 = " + sum);
-
-            if (start == end) {
-                if (sum >= K) {
-                    break;
-                } else {
-                    start += 1;
-                    break;
-                }
-            }
-
-            // 원하는 광물의 수보다 적을 경우 채굴기의 성능을 높인다.
-            if (sum < K) {
-                start = mid+1;
-            } else {
-                end = mid;
-            }
+            if (sum < K) start = mid+1;
+            else end = mid;
         }
 
+        sum = bfs(start);
+        if (sum < K) start += 1;
         System.out.println(start);
     }
 
@@ -66,18 +50,12 @@ public class Main {
         visited = new boolean[N][M];
         ArrayDeque<int[]> Q = new ArrayDeque<>();
 
-        for (int j = 0; j < M; j++) {
-            if (board[0][j] <= d) {
-                Q.addLast(new int[]{0, j});
-            }
-        }
+        for (int j = 0; j < M; j++)
+            if (board[0][j] <= d) Q.addLast(new int[]{0, j});
+
         for (int i = 1; i < N; i++) {
-            if (board[i][0] <= d) {
-                Q.addLast(new int[]{i, 0});
-            }
-            if (board[i][M-1] <= d) {
-                Q.addLast(new int[]{i, M-1});
-            }
+            if (board[i][0] <= d) Q.addLast(new int[]{i, 0});
+            if (board[i][M-1] <= d) Q.addLast(new int[]{i, M-1});
         }
 
         int cnt = 0;
@@ -94,9 +72,9 @@ public class Main {
             for (int i = 0; i < 4; i++) {
                 int nr = r + dr[i];
                 int nc = c + dc[i];
-                if (nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc] && board[nr][nc] <= d) {
+
+                if (nr >= 0 && nr < N && nc >= 0 && nc < M && !visited[nr][nc] && board[nr][nc] <= d)
                     Q.addLast(new int[]{nr, nc});
-                }
             }
         }
 
