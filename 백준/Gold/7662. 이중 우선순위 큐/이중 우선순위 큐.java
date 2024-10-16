d7dfeae670f8465bca623283c7b7334b80b6e8fb
xo482 <122208264+xo482@users.noreply.github.com>
2024-10-16 21:12:38 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2176 ms, Memory: 342728 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -1,50 +1,46 @@
+import com.sun.source.tree.Tree;
+
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
 
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static int T;
+
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        int T = Integer.parseInt(br.readLine());
+        T = Integer.parseInt(br.readLine());
         while (T-- > 0) {
-            int N = Integer.parseInt(br.readLine());
-            TreeMap<Integer, Integer> map = new TreeMap<>();
+            int k = Integer.parseInt(br.readLine());
+            TreeMap<Long, Integer> map = new TreeMap<>();
 
-            while (N-- > 0) {
-                String[] s = br.readLine().split(" ");
-                int num = Integer.parseInt(s[1]);
-                if (s[0].equals("I")){
-                    if (map.containsKey(num))
-                        map.put(num, map.get(num)+1);
-                    else
-                        map.put(num, 0);
+            while (k-- > 0) {
+                st = new StringTokenizer(br.readLine());
+
+                if (st.nextToken().charAt(0) == 'I') {
+                    Long key = Long.parseLong(st.nextToken());
+                    map.put(key, map.getOrDefault(key,0)+1);
                 }
-                else if (!map.isEmpty()) {
-                    if (num == -1) {
-                        Integer i = map.firstKey();
+                else {
+                    if (map.isEmpty()) continue;
 
-                        if (map.get(i) != 0)
-                            map.put(i, map.get(i) - 1);
-                        else
-                            map.remove(i);
-                    } else {
-                        Integer i = map.lastKey();
+                    Long key;
 
-                        if (map.get(i) != 0)
-                            map.put(i, map.get(i) - 1);
-                        else
-                            map.remove(i);
-                    }
+                    if (st.nextToken().charAt(0) == '1') key = map.lastKey();
+                    else key = map.firstKey();
+
+                    if (map.get(key) == 1) map.remove(key);
+                    else map.put(key, map.get(key) - 1);
                 }
             }
-            if (map.isEmpty()) sb.append("EMPTY\n");
-            else {
-                sb.append(map.lastKey()).append(" ").append(map.firstKey()).append("\n");
-            }
+
+            if (map.isEmpty()) sb.append("EMPTY");
+            else sb.append(map.lastKey()).append(" ").append(map.firstKey());
+            sb.append("\n");
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
     }
 }
