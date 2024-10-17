e31d34ff7014813d449d7bc5011f8d3e965b716d
xo482 <122208264+xo482@users.noreply.github.com>
2024-10-18 04:11:29 +0900

    [Gold III] Title: 아기 상어, Time: 184 ms, Memory: 22996 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -4,39 +4,30 @@ import java.util.*;
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int N, departR, departC, arriveR, arriveC;
-    static int size = 2, sizeCnt = 0;
+    static int N, R, C;
+    static int size = 2, sizeCnt = 0, total = 0;
     static int[][] adj;
     static int[] dr = new int[]{1, -1, 0, 0};
     static int[] dc = new int[]{0, 0, -1, 1};
-    static List<int[]> list = new ArrayList<>(); // 물고기들의 위치를 담고 있있음
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
 
-        // 물고기 배열 초기화
         adj = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 adj[i][j] = Integer.parseInt(st.nextToken());
                 if (adj[i][j] == 9) {
-                    departR = i;
-                    departC = j;
-                } else if (adj[i][j] != 0) list.add(new int[]{i, j});
+                    R = i;
+                    C = j;
+                }
             }
         }
 
-        int total = 0;
         while (true) {
-
-//            if (adj[arriveR][arriveC] >= size) break;
-
-            int[] answer = bfs(departR, departC);
+            int[] answer = bfs(R, C);
             if (answer[2] == -99) break;
-//            System.out.print("nextR = " + answer[0]);
-//            System.out.print(", nextC = " + answer[1]);
-//            System.out.println(", time = " + answer[2]);
             total+=answer[2];
 
             sizeCnt++;
@@ -44,8 +35,8 @@ public class Main {
                 sizeCnt = 0;
                 size++;
             }
-            departR = answer[0];
-            departC = answer[1];
+            R = answer[0];
+            C = answer[1];
         }
         System.out.println(total);
     }
@@ -59,20 +50,11 @@ public class Main {
 
                 // 만약 거리가 같다면
                 if (o1[2] == o2[2]){
-
                     // 만약 높이가 같다면
-                    if (o2[0] == o1[0]) {
-
-                        // 더 왼쪽에 있는걸 먹음
-                        return o1[1] - o2[1];
-                    }
-
-                    // 더 위에 있는걸 먹음
-                    return o1[0] - o2[0];
+                    if (o2[0] == o1[0]) return o1[1] - o2[1]; // 더 왼쪽에 있는걸 먹음
+                    return o1[0] - o2[0]; // 더 위에 있는걸 먹음
                 }
-
-                // 가장 가까운 순으로 정렬
-                return o1[2] - o2[2];
+                return o1[2] - o2[2]; // 가장 가까운 순으로 정렬
             }
         });
 
@@ -86,9 +68,7 @@ public class Main {
             int r = now[0];
             int c = now[1];
 
-            if (adj[r][c] < size && adj[r][c] != 0) {
-                pq.add(new int[]{r, c, visited[r][c]-1});
-            }
+            if (adj[r][c] < size && adj[r][c] != 0) pq.add(new int[]{r, c, visited[r][c]-1});
 
             for (int i = 0; i < 4; i++) {
                 int nr = r + dr[i];
