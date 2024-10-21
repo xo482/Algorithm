3736bf9fd855448c1be2276fc8fb8eddd7075e6b
xo482 <122208264+xo482@users.noreply.github.com>
2024-10-21 19:55:18 +0900

    [Silver I] Title: 스티커, Time: 644 ms, Memory: 103312 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,44 +1,42 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static int T;
-    static int N;
-    static int[][] dp;
-
+    static int T, n;
+    static int[][] board;
 
     public static void main(String[] args) throws IOException {
         T = Integer.parseInt(br.readLine());
-
         while (T-- > 0) {
-            N = Integer.parseInt(br.readLine());
-            dp = new int[2][N];
 
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) dp[0][i] = Integer.parseInt(st.nextToken());
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) dp[1][i] = Integer.parseInt(st.nextToken());
+            // init
+            n = Integer.parseInt(br.readLine());
+            board = new int[2][n];
+            for (int i = 0; i < 2; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < n; j++) board[i][j] = Integer.parseInt(st.nextToken());
+            }
 
-            if (N == 1){
-                sb.append(Math.max(dp[0][0], dp[1][0])).append("\n");
+
+            if (n == 1) {
+                sb.append(Math.max(board[0][0], board[1][0])).append("\n");
                 continue;
             }
 
-            dp[0][1] += dp[1][0];
-            dp[1][1] += dp[0][0];
+            // logic
+            board[0][1] += board[1][0];
+            board[1][1] += board[0][0];
 
-            for (int i = 2; i < N; i++) {
-                dp[0][i] += Math.max(dp[1][i - 1], dp[1][i - 2]);
-                dp[1][i] += Math.max(dp[0][i - 1], dp[0][i - 2]);
+            for (int j = 2; j < n; j++) {
+                board[0][j] += Math.max(board[1][j - 2], board[1][j - 1]);
+                board[1][j] += Math.max(board[0][j - 2], board[0][j - 1]);
             }
-            sb.append(Math.max(dp[0][N-1], dp[1][N-1])).append("\n");
+            sb.append(Math.max(board[0][n-1], board[1][n-1])).append("\n");
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
     }
 }
