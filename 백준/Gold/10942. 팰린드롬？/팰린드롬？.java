a9369c791ad0baf2dce387cce6a831424cad9a4f
xo482 <122208264+xo482@users.noreply.github.com>
2024-10-31 17:47:41 +0900

    [Gold IV] Title: 팰린드롬?, Time: 764 ms, Memory: 229872 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -5,38 +5,33 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static Set<String> set = new HashSet<>();
-    static int N, M;
+    static int N, Q;
     static int[] arr;
+    static int[][] dp;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
-        arr = new int[N];
+        arr = new int[N + 1];
+        dp = new int[N + 1][N + 1];
+
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) arr[i] = Integer.parseInt(st.nextToken());
 
+        for (int i = 1; i <= N; i++) dp[i][i] = 1;
+        for (int i = 1; i <= N-1; i++) if (arr[i] == arr[i+1]) dp[i][i+1] = 1;
 
-        for (int i = 0; i < N; i++) {
-            for (int j = i; j < N; j++) {
-                int s = i;
-                int e = j;
+        for (int i = 2; i <= N; i++)
+            for (int j = 1; i+j <= N; j++)
+                if (arr[j] == arr[i+j] && dp[j+1][i+j-1] == 1)
+                    dp[j][i+j] = 1;
 
-                boolean flag = false;
-                while (s <= e){
-                    if (arr[s] != arr[e]) { flag = true; break; }
-                    s++; e--;
-                }
-                if (!flag) set.add((i+1)+" "+(j+1));
-            }
+        Q = Integer.parseInt(br.readLine());
+        while (Q-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            sb.append(dp[a][b]).append("\n");
         }
-
-
-        M = Integer.parseInt(br.readLine());
-        for (int i = 0; i < M; i++) {
-            if (set.contains(br.readLine())) sb.append(1);
-            else sb.append(0);
-            sb.append("\n");
-        }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
