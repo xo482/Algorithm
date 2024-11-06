e9b19b9f8d1aab137dc14bded9db78fa92ae3c05
xo482 <122208264+xo482@users.noreply.github.com>
2024-11-06 19:32:38 +0900

    [Silver I] Title: 절댓값 힙, Time: 300 ms, Memory: 25836 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -3,38 +3,28 @@ import java.util.*;
 
 public class Main {
 
-    static int[][] arr;
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
     static int N;
+    static PriorityQueue<Integer> Q = new PriorityQueue<>(new Comparator<Integer>() {
+        @Override
+        public int compare(Integer o1, Integer o2) {
+            if (Math.abs(o1) == Math.abs(o2)) return o1 - o2;
+            return Math.abs(o1) - Math.abs(o2);
+        }
+    });
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         N = Integer.parseInt(br.readLine());
-        Queue<Integer> queue = new PriorityQueue<>(new Comparator<Integer>() {
-            @Override
-            public int compare(Integer o1, Integer o2) {
-                if (Math.abs(o1) == Math.abs(o2))
-                    return o1 - o2;
-                return Math.abs(o1) - Math.abs(o2);
-            }
-        });
-
         while (N-- > 0) {
-            int num = Integer.parseInt(br.readLine());
-
-            if (num == 0) {
-                if (queue.isEmpty()) {
-                    sb.append("0\n");
-                    continue;
-                }
-
-                sb.append(queue.remove()).append("\n");
-
-            } else {
-                queue.add(num);
+            int cal = Integer.parseInt(br.readLine());
+            if (cal == 0){
+                if (Q.isEmpty()) sb.append("0\n");
+                if (!Q.isEmpty()) sb.append(Q.poll()).append("\n");
+                continue;
             }
+            Q.add(cal);
         }
-
-        System.out.println(sb);
+        System.out.println(sb.toString());
     }
 }
