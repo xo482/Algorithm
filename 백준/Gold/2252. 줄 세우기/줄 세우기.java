a3e29aad29ad70a22c316019518be13726ebff51
xo482 <122208264+xo482@users.noreply.github.com>
2025-01-14 15:15:19 +0900

    [Gold III] Title: 줄 세우기, Time: 416 ms, Memory: 44996 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,14 +1,13 @@
 import java.io.*;
 import java.util.*;
 
-// 앞에 선다 -> 먼저 나온다
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
     static int N, M;
     static int[] inDegree;
     static List<Integer>[] list;
-    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
@@ -16,27 +15,35 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         inDegree = new int[N + 1];
         list = new List[N + 1];
-        for (int i = 0; i < N + 1; i++) list[i] = new ArrayList<>();
+        for (int i = 1; i < N + 1; i++) list[i] = new ArrayList<>();
 
         while (M-- > 0) {
             st = new StringTokenizer(br.readLine());
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
-            inDegree[B]++;
+
             list[A].add(B);
+            inDegree[B]++;
         }
 
+        // logic
         ArrayDeque<Integer> Q = new ArrayDeque<>();
-        for (int i = 1; i < N + 1; i++) if (inDegree[i] == 0) Q.addLast(i);
+        for (int i = 1; i < N + 1; i++)
+            if (inDegree[i] == 0) {
+                Q.add(i);
+                sb.append(i).append(" ");
+            }
 
         while (!Q.isEmpty()) {
             int now = Q.removeFirst();
-            sb.append(now).append(" ");
-
-            for (int next : list[now]) {
-                if (--inDegree[next] == 0) Q.addLast(next);
+            for (int nxt : list[now]) {
+                if (--inDegree[nxt] == 0) {
+                    Q.add(nxt);
+                    sb.append(nxt).append(" ");
+                }
             }
         }
+
         System.out.println(sb);
     }
 }
