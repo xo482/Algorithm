3f13a091cbd87a8f97f2cf43aa7934124ecd043c
xo482 <122208264+xo482@users.noreply.github.com>
2025-01-24 17:37:42 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 11.98 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,12 +1,16 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
-    public String solution(String s) {
-        int[] arr = Arrays.stream(s.split(" "))
-            .mapToInt(Integer::parseInt)
-            .sorted()
-            .toArray();
+    public String solution(String str) {
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        String[] arr = str.split(" ");
+        for(String s : arr) {
+            int now = Integer.parseInt(s);
+            min = Math.min(min, now);
+            max = Math.max(max, now);
+        }
         
-        return arr[0] + " " + arr[arr.length - 1];
+        return min + " " + max;
     }
 }
