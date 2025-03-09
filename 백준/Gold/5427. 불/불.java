4495d1be157a45b4d8b6d07b1d8ad51b6a945684
xo482 <122208264+xo482@users.noreply.github.com>
2025-03-09 14:23:07 +0900

    [Gold IV] Title: 불, Time: 712 ms, Memory: 76788 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -3,13 +3,13 @@ import java.util.*;
 
 public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
+    static StringBuilder sb = new StringBuilder();
     static int T, N, M;
+    static ArrayDeque<Point> fireQ, personQ;
     static char[][] board;
     static int[] dr = new int[]{1, -1, 0, 0};
     static int[] dc = new int[]{0, 0, -1, 1};
-
 
     public static void main(String[] args) throws IOException {
         T = Integer.parseInt(br.readLine());
@@ -17,97 +17,73 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
             N = Integer.parseInt(st.nextToken());
+
             board = new char[N][M];
-
-            ArrayDeque<Unit> fireQ = new ArrayDeque<>();
-            ArrayDeque<Unit> UserQ = new ArrayDeque<>();
-
+            fireQ = new ArrayDeque<>();
+            personQ = new ArrayDeque<>();
 
             for (int i = 0; i < N; i++) {
-                char[] chars = br.readLine().toCharArray();
+                char[] charArray = br.readLine().toCharArray();
                 for (int j = 0; j < M; j++) {
-                    board[i][j] = chars[j];
-                    if (board[i][j] == '@') UserQ.add(new Unit(i, j, 0));
-                    if (board[i][j] == '*') fireQ.add(new Unit(i, j, 0));
+                    board[i][j] = charArray[j];
+                    if (board[i][j] == '*') fireQ.add(new Point(i, j));
+                    if (board[i][j] == '@') personQ.add(new Point(i, j));
                 }
             }
 
-            boolean check = false;
-            while (!(UserQ.isEmpty() && fireQ.isEmpty())) {
-                if (UserQ.isEmpty()) break;
-                int size = UserQ.size();
+            int cnt = 0;
+            boolean flag = false;
+            while (!personQ.isEmpty()) {
+                int size = fireQ.size();
                 for (int i = 0; i < size; i++) {
-                    Unit now = UserQ.removeFirst();
-                    for (int j = 0; j < 4; j++) {
-                        int nr = now.r + dr[j];
-                        int nc = now.c + dc[j];
-
-                        if (nr >= N || nr < 0 || nc >= M || nc < 0) {
-                            sb.append(now.time + 1).append("\n");
-                            check = true;
-                            break;
-                        }
-                        if (board[nr][nc] != '.') continue;
-
-                        boolean flag = false;
-                        for (int k = 0; k < 4; k++) {
-                            int nnr = nr + dr[k];
-                            int nnc = nc + dc[k];
-
-                            if (nnr >= N || nnr < 0 || nnc >= M || nnc < 0) continue;
-                            if (board[nnr][nnc] != '*') continue;
-
-                            flag = true;
-                        }
-                        if (flag) continue;
-
-                        board[nr][nc] = '@';
-                        UserQ.addLast(new Unit(nr, nc, now.time + 1));
-                    }
-                    if (check) break;
-                }
-                if (check) break;
-
-
-                if (fireQ.isEmpty()) continue;
-                size = fireQ.size();
-                for (int i = 0; i < size; i++) {
-                    Unit now = fireQ.removeFirst();
+                    Point now = fireQ.removeFirst();
 
                     for (int j = 0; j < 4; j++) {
                         int nr = now.r + dr[j];
                         int nc = now.c + dc[j];
 
                         if (nr >= N || nr < 0 || nc >= M || nc < 0) continue;
-                        if (board[nr][nc] == '#' || board[nr][nc] == '*') continue;
+                        if (board[nr][nc] == '#') continue;
+                        if (board[nr][nc] == '*') continue;
 
                         board[nr][nc] = '*';
-                        fireQ.add(new Unit(nr, nc, 0));
+                        fireQ.add(new Point(nr, nc));
                     }
                 }
 
+                cnt++;
+                size = personQ.size();
+                for (int i = 0; i < size; i++) {
+                    Point now = personQ.removeFirst();
 
+                    for (int j = 0; j < 4; j++) {
+                        int nr = now.r + dr[j];
+                        int nc = now.c + dc[j];
+
+
+                        if (nr >= N || nr < 0 || nc >= M || nc < 0) { flag = true; break; }
+                        if (board[nr][nc] == '.') {
+                            board[nr][nc] = '@';
+                            personQ.add(new Point(nr, nc));
+                        }
+                    }
+                    if (flag) break;
+                }
+                if (flag) break;
             }
-            if (!check) sb.append("IMPOSSIBLE\n");;
+
+            if (flag) sb.append(cnt).append("\n");
+            else sb.append("IMPOSSIBLE\n");
         }
         System.out.println(sb);
-
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 0; j < M; j++) {
-//                System.out.print(board[i][j] + " ");
-//            }
-//            System.out.println();
-//        }
     }
-    static class Unit {
 
+    static class Point{
         int r, c;
-        int time;
 
-        public Unit(int r, int c, int time) {
+        public Point(int r, int c) {
             this.r = r;
             this.c = c;
-            this.time = time;
         }
     }
 }
