39dae53668e9fc1aee1e7520c8430121ddb0f944
xo482 <122208264+xo482@users.noreply.github.com>
2025-03-15 14:23:46 +0900

    [Silver I] Title: 곱셈, Time: 112 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.java
+++ 백준/Silver/1629. 곱셈/곱셈.java
@@ -5,24 +5,28 @@ public class Main {
 
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
-    static int A;
-    static int B;
-    static int C;
-    public static void main(String[] args) throws IOException {
+    static int A,B,C;
 
+    public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         A = Integer.parseInt(st.nextToken());
         B = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
         System.out.print(method(A, B));
+
+//
+//        7^8 % 5 = (7^4%5 * 7^4%5) % 5
+//        7^9 % 5 =  (7^8%5 * 7%5)%5 =  ((7^4%5 + 7^4%5) % 5 * 7%5)%5
+//
+//            7^9 = 7^8 * 7
     }
 
     private static long method(int a, int b) {
         if (b == 1) return a % C;
 
-        long answer = method(a, b / 2);
-        if (b % 2 == 0) return answer * answer % C;
-        return (answer * answer % C) * a % C;
+        long temp = method(a,b/2);
+        if (b % 2 == 0) return temp * temp % C;
+        return (temp * temp % C * a % C) % C;
     }
 }
