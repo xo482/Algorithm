fc43c03e35e31e9aef03dd5aab871fb82c256f0b
xo482 <122208264+xo482@users.noreply.github.com>
2025-03-28 22:44:36 +0900

    [Gold IV] Title: 북쪽나라의 도로, Time: 440 ms, Memory: 22640 KB -BaekjoonHub

--- 백준/Gold/1595. 북쪽나라의 도로/북쪽나라의 도로.java
+++ 백준/Gold/1595. 북쪽나라의 도로/북쪽나라의 도로.java
@@ -9,10 +9,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        for (int i = 0; i < 10000; i++) {
-            list[i] = new ArrayList<>();
-        }
-
+        for (int i = 0; i < 10000; i++) list[i] = new ArrayList<>();
 
         while (true) {
             String str = br.readLine();
@@ -28,21 +25,18 @@ public class Main {
             nodeCount++;
         }
 
-        for (int i = 0; i < nodeCount; i++) {
-            dfs(i+1, 0, 0);
-        }
+        for (int i = 0; i < nodeCount; i++) dfs(i+1, 0, 0);
 
         System.out.println(max);
     }
 
     private static void dfs(int now, int pre, int sum) {
-        if (list[now].size() == 1 && list[now].get(0)[0] == pre) {
-            max = Math.max(max, sum);
-            return;
-        }
-
         for (int[] nxt : list[now]) {
             if (nxt[0] == pre) continue;
+            if (list[nxt[0]].size() == 1) {
+                max = Math.max(max, sum + nxt[1]);
+                continue;
+            }
             dfs(nxt[0], now,sum + nxt[1]);
         }
     }
