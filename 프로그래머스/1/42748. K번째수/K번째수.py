ea429a43823f21188b87e04d681eed5518d652d9
xo482 <122208264+xo482@users.noreply.github.com>
2025-04-27 16:35:19 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,10 +1,4 @@
 def solution(array, commands):
+    return [sorted(array[i-1:j])[k-1] for i,j,k in commands]    
     
-    ans = []
     
-    for i,j,k in commands:
-        ls = array[i-1:j]
-        ls.sort()
-        ans.append(ls[k-1])
-    
-    return ans
