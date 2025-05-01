bb0ab7c3546dc682d9d4cfb53cc8c76cf3d3af0e
xo482 <122208264+xo482@users.noreply.github.com>
2025-05-01 19:19:11 +0900

    [level 1] Title: 문자열 나누기, Time: 1.87 ms, Memory: 9.09 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.py
@@ -1,23 +1,17 @@
 def solution(s):
-    ls = list(s)
     ans = 0
     
-    first = "0"
+    first = None
     sc = 0
     dc = 0
     
-    for v in ls:
-        if first == "0":
-            first = v
-        
-            
-        if first == v:
-            sc += 1
-        if first != v:
-            dc += 1
+    for v in s:
+        if first is None: first = v
+        if first == v: sc += 1
+        if first != v: dc += 1
             
         if sc == dc:
-            first = "0"
+            first = None
             sc = 0
             dc = 0
             ans += 1
