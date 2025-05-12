bafd0b9bff31a8fdb6df970662429fddf7ea4fad
xo482 <122208264+xo482@users.noreply.github.com>
2025-05-13 01:41:55 +0900

    [Silver I] Title: 안전 영역, Time: 632 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,30 +1,26 @@
 from collections import deque
 
-dr = [-1,1,0,0]
-dc = [0,0,1,-1]
+dr = [-1, 1, 0, 0]
+dc = [0, 0, 1, -1]
 n = int(input())
 board = [list(map(int, input().split())) for i in range(n)]
+max_value = max([max(row) for row in board])
 
 def bfs(si, sj, h, visited):
-    q = deque()
-    q.append([si,sj])
+    q = deque([(si, sj)])
     visited[si][sj] = True
 
     while q:
-        now = q.popleft()
-        r = now[0]
-        c = now[1]
-
+        r, c = q.popleft()
         for i in range(4):
             nr = r + dr[i]
             nc = c + dc[i]
 
             if nr < 0 or nr >= n or nc < 0 or nc >= n: continue
-
             if board[nr][nc] <= h: continue
             if visited[nr][nc]: continue
 
-            q.append([nr,nc])
+            q.append((nr, nc))
             visited[nr][nc] = True
 
 def solution(h):
@@ -34,13 +30,9 @@ def solution(h):
     for i in range(n):
         for j in range(n):
             if board[i][j] > h and not visited[i][j]:
-                cnt+=1
+                cnt += 1
                 bfs(i, j, h, visited)
 
     return cnt
 
-
-
-max_value = max([max(row) for row in board])
-print(max([solution(i) for i in range(0, max_value+1)]))
-
+print(max([solution(i) for i in range(max_value + 1)]))
