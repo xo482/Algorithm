29ce3573b364ffa17bffecbfcbca311bd4644567
xo482 <122208264+xo482@users.noreply.github.com>
2025-05-13 01:26:02 +0900

    [Silver I] Title: 안전 영역, Time: 664 ms, Memory: 34984 KB -BaekjoonHub

---
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -0,0 +1,46 @@
+from collections import deque
+
+dr = [-1,1,0,0]
+dc = [0,0,1,-1]
+n = int(input())
+board = [list(map(int, input().split())) for i in range(n)]
+
+def bfs(si, sj, h, visited):
+    q = deque()
+    q.append([si,sj])
+    visited[si][sj] = True
+
+    while q:
+        now = q.popleft()
+        r = now[0]
+        c = now[1]
+
+        for i in range(4):
+            nr = r + dr[i]
+            nc = c + dc[i]
+
+            if nr < 0 or nr >= n or nc < 0 or nc >= n: continue
+
+            if board[nr][nc] <= h: continue
+            if visited[nr][nc]: continue
+
+            q.append([nr,nc])
+            visited[nr][nc] = True
+
+def solution(h):
+    cnt = 0
+    visited = [[False] * n for i in range(n)]
+
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] > h and not visited[i][j]:
+                cnt+=1
+                bfs(i, j, h, visited)
+
+    return cnt
+
+
+
+max_value = max([max(row) for row in board])
+print(max([solution(i) for i in range(0, max_value+1)]))
+
