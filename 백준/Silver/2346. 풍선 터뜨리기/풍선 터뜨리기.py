47b003a3124dd7536d56dd6a98e895b39d060bf0
xo482 <122208264+xo482@users.noreply.github.com>
2025-06-09 02:01:04 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 52 ms, Memory: 34916 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.py
@@ -3,31 +3,9 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-ls = list(map(int, input().split()))
-visited = [False] * n
+q = deque((i+1, mv) for i, mv in enumerate(map(int, input().split())))
 
-idx = 0
-cnt = 0
-while True:
-    print(idx+1, end=' ')
-    visited[idx] = True
-    mv = ls[idx]
-    cnt += 1
-
-    if cnt == n:
-        break
-
-    c = 0
-    if mv > 0:
-        while c != mv:
-            idx = (idx + 1) % n
-            if visited[idx] == True:
-                continue
-            c += 1
-
-    if mv < 0:
-        while c != mv:
-            idx = (idx - 1 + n) % n
-            if visited[idx] == True:
-                continue
-            c -= 1
+while q:
+    i, mv = q.popleft()
+    print(i, end=' ')
+    q.rotate(-(mv if mv < 0 else mv - 1))
