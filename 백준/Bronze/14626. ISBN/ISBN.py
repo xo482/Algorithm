5a17e4da39a5eb7de352d72655e80a972eaa6141
xo482 <122208264+xo482@users.noreply.github.com>
2025-06-15 00:55:50 +0900

    [Bronze I] Title: ISBN, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14626. ISBN/ISBN.py
+++ 백준/Bronze/14626. ISBN/ISBN.py
@@ -1,29 +1,14 @@
-import sys
-input = sys.stdin.readline
-
-s = 0
-idx = -1
-ls = list(input())
-m = int(ls[12])
+line = input()
+m = int(line[12])
+idx = line.index('*')
+weight = [1, 3] * 6
+su = 0
 
 for i in range(12):
-    if ls[i] == '*':
-        idx = i
-        continue
-
-    num = int(ls[i])
-    s += num
-
-    if i % 2 == 1:
-        s += 2 * num
+    if line[i] == '*': continue
+    su += int(line[i]) * weight[i]
 
 for v in range(10):
-    if idx % 2 == 1:
-        if ((s + 3 * v) % 10 + m) % 10 == 0:
-            print(v)
-            exit()
-
-    if idx % 2 == 0:
-        if ((s + v) % 10 + m) % 10 == 0:
-            print(v)
-            exit()
+    if ((su + v * weight[idx]) % 10 + m) % 10 == 0:
+        print(v)
+        exit()
