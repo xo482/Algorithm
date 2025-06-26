e915ec4b6ceb565d7ee8567587ca46322fe664af
xo482 <122208264+xo482@users.noreply.github.com>
2025-06-26 18:47:16 +0900

    [Silver III] Title: N번째 큰 수, Time: 4968 ms, Memory: 38984 KB -BaekjoonHub

---
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -0,0 +1,16 @@
+from queue import PriorityQueue
+q = PriorityQueue()
+n = int(input())
+cnt = 0
+
+for _ in range(n):
+    for v in list(map(int, input().split())):
+        if cnt != n: 
+            cnt += 1
+            q.put(v)
+            continue
+            
+        q.put(v)    
+        q.get()
+
+print(q.get())
