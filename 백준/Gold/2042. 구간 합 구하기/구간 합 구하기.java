07b2a6b22b6af442bb2d357c524769a349ffe9a5
xo482 <122208264+xo482@users.noreply.github.com>
2025-09-26 16:26:15 +0900

    [Gold I] Title: 구간 합 구하기, Time: 516 ms, Memory: 94020 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -5,72 +5,83 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringBuilder sb = new StringBuilder();
     static StringTokenizer st;
-    static int N , M;
+    static int N, M, K, a, b, startIdx;
+    static long c;
     static long[] tree;
 
     public static void main(String[] args) throws IOException {
-        st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        M += Integer.parseInt(st.nextToken());
+        init();
 
-        int treeSize = getTreeSize();
-        tree = new long[treeSize];
-
-        int start_idx = treeSize / 2;
-        for (int i = 0; i < N; i++) tree[start_idx + i] = Long.parseLong(br.readLine());
-
-        init(start_idx);
-
-        while (M-- > 0) {
-            st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken()) + start_idx - 1;
-            long c = Long.parseLong(st.nextToken());
+        for(int i = 0; i < M+K; i++) {
+            setABC();
 
             if (a == 1) update(b, c);
-            if (a == 2) sb.append(query(b, (int)c + start_idx - 1)).append("\n");
+            if (a == 2) {
+                long sum = query(b, (int)c);
+                sb.append(sum).append('\n');
+            }
         }
         System.out.print(sb);
     }
 
-    private static void update(int idx, long value) {
-        long diff = value - tree[idx];
-        tree[idx] = value;
-        while (idx > 1) {
-            tree[idx / 2] += diff;
-            idx /= 2;
-        }
-    }
-
-    private static long query(int s_idx, int e_idx) {
-
+    static long query(int s, int e) {
+        s = s + startIdx - 1;
+        e = e + startIdx - 1;
         long sum = 0;
-        while (s_idx <= e_idx) {
-            if (s_idx % 2 == 1) sum += tree[s_idx];
-            if (e_idx % 2 == 0) sum += tree[e_idx];
 
-            s_idx = (s_idx + 1) / 2;
-            e_idx = (e_idx - 1) / 2;
+        while(s <= e) {
+            if (s % 2 == 1) sum += tree[s];
+            if (e % 2 == 0) sum += tree[e];
+
+            s = (s + 1) / 2;
+            e = (e - 1) / 2;
         }
         return sum;
     }
 
-    private static void init(int start_idx) {
-        for (int i = 0; i < N; i++) {
-            int idx = start_idx + i;
-            int idx2 = idx;
+    static void update(int idx, long value) {
+        idx = idx + startIdx - 1;
+        long diff = value - tree[idx];
 
-            while (idx2 > 1) {
-                tree[idx2 / 2] += tree[idx];
-                idx2 /= 2;
+        while (idx > 0) {
+            tree[idx] += diff;
+            idx = idx / 2;
+        }
+    }
+
+    static void setABC() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Long.parseLong(st.nextToken());
+    }
+
+    static void init() throws IOException {
+        st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(st.nextToken());
+
+        int treeSize = getTreeSize();
+        tree = new long[treeSize];
+        startIdx = treeSize / 2;
+        for(int i = 0; i < N; i++) {
+            int idx = startIdx + i;
+            long value = Long.parseLong(br.readLine());
+
+            while (idx > 0) {
+                tree[idx] += value;
+                idx = idx / 2;
             }
         }
     }
 
-    private static int getTreeSize() {
-        int cnt = 0;
-        while (Math.pow(2,cnt++) < N) {}
-        return (int) Math.pow(2, cnt);
+    static int getTreeSize() {
+        int i = 0;
+        while (true) {
+            int size = (int) Math.pow(2, i++);
+            if (size >= N) break;
+        }
+        return (int) Math.pow(2, i);
     }
 }
