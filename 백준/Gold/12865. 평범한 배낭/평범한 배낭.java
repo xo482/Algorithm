04ddb732b4367c545cbeb9d5439df96e56126e68
xo482 <122208264+xo482@users.noreply.github.com>
2025-10-08 17:34:01 +0900

    [Gold V] Title: 평범한 배낭, Time: 140 ms, Memory: 15428 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -5,21 +5,23 @@ public class Main {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     static StringTokenizer st;
     static int N, K, W, V;
-    static int[] dp; // 인덱스는 무게, 요소는 해당 무게에서의 최대 가치값
+    static int[] dp;
 
     public static void main(String[] args) throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        dp = new int[K+1];
-
+        dp = new int[K + 1];
+        
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             W = Integer.parseInt(st.nextToken());
             V = Integer.parseInt(st.nextToken());
-
-            for (int j = K; j >= W; j--) dp[j] = Math.max(dp[j], dp[j - W] + V);
+            
+            for (int j = K; j >= W; j--) {
+                dp[j] = Math.max(dp[j], dp[j-W] + V);
+            }
         }
-        System.out.println(dp[K]);
+        System.out.print(dp[K]);
     }
 }
