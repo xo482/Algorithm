c07690418c943fc57a0dbc37573a97c2741c3b6b
xo482 <122208264+xo482@users.noreply.github.com>
2025-11-11 21:11:14 +0900

    [Gold IV] Title: 호숫가의 개미굴, Time: 368 ms, Memory: 45392 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.java
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.java
@@ -6,14 +6,12 @@ public class Main {
     static StringTokenizer st;
     static int N;
     static long[] arr;
-    static int[][] dp;
     static int[] visited;
     static long answer = 0;
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         arr = new long[N];
-        dp = new int[N][2];
         visited = new int[N];
 
         st = new StringTokenizer(br.readLine());
@@ -25,19 +23,27 @@ public class Main {
             }
         }
 
+        int test = 0;
         for (int i = 0; i < N; i++) {
-            if (visited[i] != 0) {
-                if (i == 0) continue;
-                dp[i][0] = dp[i-1][0];
-                dp[i][1] = dp[i-1][1];
-                continue;
-            }
-
-            dp[i][1] = dp[(i-1+N) % N][0] + 1;
-            if (visited[(i-1+N) % N] == 1) dp[i][1] = Math.max(dp[(i-1+N) % N][0], dp[(i-1+N) % N][1]) + 1;
-            dp[i][0] = Math.max(dp[(i-1+N) % N][0], dp[(i-1+N) % N][1]);
+            if (visited[i] != 0) continue;
+            if (visited[(i+1) % N] == 2) continue;
+            if (visited[(i - 1 +N) % N] == 2) continue;
+            test++;
+            visited[i] = 2;
         }
 
-        System.out.println(answer + Math.max(dp[N-1][0], dp[N-1][1]));
+        for (int i = 0; i < N; i++) if (visited[i] == 2) visited[i] = 0;
+
+        int test2 = 0;
+        for (int i = 1; i < N; i++) {
+            if (visited[i] != 0) continue;
+            if (visited[(i+1) % N] == 2) continue;
+            if (visited[(i - 1 +N) % N] == 2) continue;
+            test2++;
+            visited[i] = 2;
+        }
+
+        answer += Math.max(test, test2);
+        System.out.println(answer);
     }
 }
