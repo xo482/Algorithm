1dc1a7318ad2eefe747c103510f8be11a524d30a
xo482 <122208264+xo482@users.noreply.github.com>
2025-11-16 01:20:02 +0900

    [Gold IV] Title: 중복 없는 수, Time: 212 ms, Memory: 14388 KB -BaekjoonHub

---
+++ 백준/Gold/11037. 중복 없는 수/중복 없는 수.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static int INF = 987654321;
+    static int num, size;
+    static boolean[] visited;
+    static boolean flag;
+    static String str;
+
+    public static void main(String[] args) throws Exception {
+
+
+        while (true) {
+            str = br.readLine();
+            if (str == null || str.isEmpty()) break;
+            num = Integer.parseInt(str);
+
+            if (num >= INF) {
+                sb.append(0).append('\n');
+                continue;
+            }
+
+            size = str.length();
+            if (str.charAt(0) == '9') size++;
+
+            flag = false;
+            visited = new boolean[10];
+            tracking(0, 0);
+        }
+
+        System.out.println(sb);
+    }
+
+    private static void tracking(int n, int depth) {
+        if (depth == size) {
+            if (num < n) {
+                sb.append(n).append('\n');
+                flag = true;
+            }
+        }
+
+        for (int i = 1; i <= 9; i++) {
+            if (visited[i]) continue;
+            visited[i] = true;
+            tracking(n + i * (int) Math.pow(10, size - depth - 1), depth + 1);
+            if (flag) break;
+            visited[i] = false;
+        }
+    }
+}
