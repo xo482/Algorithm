429110637ed580e6e0ff21340e16615fce720621
xo482 <122208264+xo482@users.noreply.github.com>
2025-12-03 00:21:20 +0900

    [Gold IV] Title: 수도배관공사, Time: 320 ms, Memory: 33192 KB -BaekjoonHub

--- 백준/Gold/2073. 수도배관공사/수도배관공사.py
+++ 백준/Gold/2073. 수도배관공사/수도배관공사.py
@@ -2,14 +2,16 @@ import sys
 input = sys.stdin.readline
 
 n, p = map(int, input().split())
+arr = sorted([list(map(int, input().split())) for _ in range(p)], key=lambda x: -x[1])
+
 dp = [0 for _ in range(n+1)]
 dp[0] = 1 << 23
 
-for _ in range(p):
-    l, c = map(int, input().split())
-
+for l, c in arr:
     for i in range(n, l - 1, -1):
         if dp[i - l]:
             dp[i] = max(dp[i], min(dp[i - l], c))
+        if dp[n]: break
+    if dp[n]: break
 
 print(dp[n])
