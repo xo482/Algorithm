30d398a27ddf1e000f98c3c9cb52103219f680f4
xo482 <122208264+xo482@users.noreply.github.com>
2025-12-09 00:59:29 +0900

    [Gold IV] Title: 중복 제거, Time: 1128 ms, Memory: 316928 KB -BaekjoonHub

---
+++ 백준/Gold/13701. 중복 제거/중복 제거.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+    static BitSet bitSet = new BitSet(33554432);
+
+    public static void main(String[] args) throws Exception {
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            int x = Integer.parseInt(st.nextToken());
+
+            if (bitSet.get(x)) continue;
+            bitSet.set(x);
+            sb.append(x).append(' ');
+        }
+        System.out.print(sb);
+    }
+}
